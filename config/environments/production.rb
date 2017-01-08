e055ce3d1d256ab8654f477550548b7f7713a8d9
Lakshman <lakshmanmurugesan@gmail.com>
2017-01-08 15:17:21 +0530

    updated production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.raise_delivery_errors = true
+  
   config.action_mailer.default_url_options = {:host => 'collegeda.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
