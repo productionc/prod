0d66f5bb52466aed9d68571436acf4abcf14ca2c
Lakshman <lakshmanmurugesan@gmail.com>
2017-01-20 23:33:49 +0530

    changing event view count for testing

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,7 +17,7 @@ before_action :authenticate_user!, only: [:show, :new]
      @events = Event.joins(:event_departments, :event_college_banner, :event_detail).where(query.join(" AND ")).paginate(:page => params[:page], :per_page => 4).order("event_details.start_date ASC").select("event_name, study_place, country, state, district, event_type, event_details.start_date as start_date, event_details.end_date as end_date, event_college_banners.id as college_banner_id, events.id as id").uniq
      # binding.pry
     else
-     @events = Event.joins(:event_departments, :event_college_banner, :event_detail).where(is_published: true).paginate(:page => params[:page], :per_page => 4).order("event_details.start_date ASC").select("event_name, study_place, country, state, district, event_type, event_details.start_date as start_date, event_details.end_date as end_date, event_college_banners.id as college_banner_id, events.id as id").uniq
+     @events = Event.joins(:event_departments, :event_college_banner, :event_detail).where(is_published: true).paginate(:page => params[:page], :per_page => 1).order("event_details.start_date ASC").select("event_name, study_place, country, state, district, event_type, event_details.start_date as start_date, event_details.end_date as end_date, event_college_banners.id as college_banner_id, events.id as id").uniq
      # binding.pry
     end
 
