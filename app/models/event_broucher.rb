20fa470fc3995e214ac97072e9d4bf91d1e4408a
Lakshman <lakshmanmurugesan@gmail.com>
2017-01-21 00:41:06 +0530

    Added event broucher pdf, college banner to view page

--- app/models/event_broucher.rb
+++ app/models/event_broucher.rb
@@ -1,11 +1,11 @@
 class EventBroucher < ActiveRecord::Base
-  has_one :event
+ has_one :event
 
  has_attached_file :broucher, :styles => { :small => "150x150>" },
                   :url  => "/assets/attachments/:id/:style/:basename.:extension",
                   :path => ":rails_root/public/assets/attachments/:id/:style/:basename.:extension"
 
 validates_attachment_presence :broucher
-validates_attachment_size :broucher, :less_than => 5.megabytes
-validates_attachment_content_type :broucher, :content_type => ['image/jpeg', 'image/png']
+validates_attachment_size :broucher, :less_than => 30.megabytes
+validates_attachment_content_type :broucher, :content_type => ['application/pdf']
 end
