f2334104ba597bd1f158cd42842e502ebbdc1204
Lakshman <lakshmanmurugesan@gmail.com>
2017-02-19 19:22:52 +0530

    updating mailer url in prod

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = {:host => 'collegedaa.com'}
+  config.action_mailer.default_url_options = {:host => 'staging-collegedaa.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :user_name => 'collegedaa',
