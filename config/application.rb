ecbbc8c2e143bf475b348b471843cabc26754b66
Lakshman <lakshmanmurugesan@gmail.com>
2017-03-18 22:43:57 +0530

    Reset config

--- config/application.rb
+++ config/application.rb
@@ -22,9 +22,6 @@ module Collegedaa
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.middleware.delete Rack::Lock
-    config.middleware.use FayeRails::Middleware, mount: '/faye', :timeout => 25
-
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
