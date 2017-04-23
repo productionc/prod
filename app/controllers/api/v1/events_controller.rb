1facfc859ae3729387db6b0c4b7ea0b47d949b61
Lakshman <lakshmanmurugesan@gmail.com>
2017-04-23 13:46:43 +0530

    Added per page, no of pages to get all events api

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,12 +2,14 @@ class Api::V1::EventsController < Api::V1::BaseController
 
   def events_list
     all_events = Event.joins(:event_departments, :event_college_banner, :event_detail).order("created_at desc")
-    paginated_events = all_events.paginate(page: params[:page].to_i, per_page: 10)
+    per_page = 10
+    paginated_events = all_events.paginate(page: params[:page].to_i, per_page: per_page)
     total_entries = paginated_events.total_entries
+    no_of_pages = total_entries/per_page
     has_more = paginated_events.current_page != paginated_events.total_pages && paginated_events.current_page <= paginated_events.total_pages
     code = paginated_events.count > 0 ? 200 : 400
     message = paginated_events.count > 0 ? "Successful" : "Unsuccessful"
-    render json: paginated_events, each_serializer: Api::V1::Serializers::EventSerializer, root: 'events', meta: {status: code, message: message, has_more: has_more, total_entries: total_entries}
+    render json: paginated_events, each_serializer: Api::V1::Serializers::EventSerializer, root: 'events', meta: {status: code, message: message, has_more: has_more, per_page: per_page, no_of_pages: no_of_pages,total_entries: total_entries}
   end
 
   def events_count
